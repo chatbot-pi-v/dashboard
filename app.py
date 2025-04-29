f5f48026f5c9d6129a2236d2de312d87dad235eb
Luisa Moraes <luisamoraes221@gmail.com>
2025-04-29 20:57:24 -0300

    adding cors and routes to upload files

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import time
 from settings_folders import AUDIO_DIR, DOCS_DIR, PDF_DIR
 from flask import Flask, request, jsonify
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 start_time = time.time()
 
@@ -14,7 +16,25 @@ DOCS_DIR.mkdir(exist_ok=True)
 AUDIO_DIR.mkdir(exist_ok=True)
 PDF_DIR.mkdir(exist_ok=True)
 
-@app.route('/upload', methods=['POST'])
+@app.route('/uploadfile', methods=['POST'])
+def upload_file():
+    print("Iniciando o upload do arquivo")
+    if 'file' not in request.files:
+        return {"error": "Nenhum arquivo enviado"}, 400
+
+    file = request.files['file']
+    print(file)
+    if file.filename == '':
+        return {"error": "Nome do arquivo vazio"}, 400
+
+    # Opcional: salvar o arquivo
+    file.save(f"./docs/audio/{file.filename}")
+    
+    runApp("maluzinha")
+    
+    return {"filename": file.filename, "message": "Arquivo recebido com sucesso!"}
+
+@app.route('/uploadlink', methods=['POST'])
 def receber_json():
     dados = request.get_json()
 
@@ -23,9 +43,7 @@ def receber_json():
 
     download_to_mp3(url)
 
-    run_transcription()
-
-    initialize_milvus(quote)
+    runApp(quote)
 
     end_time = time.time()
 
@@ -37,6 +55,11 @@ def receber_json():
         "execution_time_seconds": int(execution_time)
         }), 200
 
+def runApp(quote):
+    run_transcription()
+    initialize_milvus(quote)
+    
+    
 if __name__ == '__main__':
-    print("Running in 127.0.0.1:6000")
-    app.run(host='0.0.0.0', port=6000, debug=True)
+    print("Running in 127.0.0.1:8000")
+    app.run(host='0.0.0.0', port=8000, debug=True)
