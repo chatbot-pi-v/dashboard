009604f5e21f8ac24292ecc92c24f9dfab0ca004
Paulo Henrique Meneghini <paulo.meneghini@outlook.com>
2025-05-15 21:27:50 -0300

    feat: create image dir

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from milvus.milvus_init import initialize_milvus
 from transformLinkInMP3.main import download_to_mp3
 from audioToTextWhisper.main import run_transcription
 import time
-from settings_folders import AUDIO_DIR, DOCS_DIR, PDF_DIR
+from settings_folders import AUDIO_DIR, DOCS_DIR, IMG_DIR, PDF_DIR
 from flask import Flask, request, jsonify
 import os
 from flask_cors import CORS
@@ -15,18 +15,23 @@ start_time = time.time()
 DOCS_DIR.mkdir(exist_ok=True)
 AUDIO_DIR.mkdir(exist_ok=True)
 PDF_DIR.mkdir(exist_ok=True)
+IMG_DIR.mkdir(exist_ok=True)
 
 @app.route('/uploadfile', methods=['POST'])
 def upload_file():
     print("Iniciando o upload dos arquivos")
 
+    pasta = request.form.get('caminho')
+    print(f"Caminho: {pasta}")
+
     files = request.files.getlist('files')
+
     citacoes = request.form.getlist('citacoes')
 
     if not files:
         return {"error": "Nenhum arquivo enviado"}, 400
 
-    os.makedirs("./docs/audio", exist_ok=True)
+    os.makedirs(pasta, exist_ok=True)
 
     for i in range(len(files)):
         file = files[i]
@@ -35,13 +40,40 @@ def upload_file():
         if file.filename == '':
             continue
         
-        caminho = f"./docs/audio/{file.filename}"
+        caminho = f"{pasta}/{file.filename}"
         file.save(caminho)
         print(f"Arquivo salvo: {file.filename}, Citação: {citacao}")
 
-        runApp(citacao)
+        # runApp(citacao)
 
     return {"message": "Todos os arquivos foram recebidos com sucesso!"}
+
+# @app.route('/uploadfile', methods=['POST'])
+# def upload_file(caminho):
+#     print("Iniciando o upload dos arquivos")
+
+#     files = request.files.getlist('files')
+#     citacoes = request.form.getlist('citacoes')
+
+#     if not files:
+#         return {"error": "Nenhum arquivo enviado"}, 400
+
+#     os.makedirs("./docs/audio", exist_ok=True)
+
+#     for i in range(len(files)):
+#         file = files[i]
+#         citacao = citacoes[i] if i < len(citacoes) else ''
+        
+#         if file.filename == '':
+#             continue
+        
+#         caminho = f"./docs/audio/{file.filename}"
+#         file.save(caminho)
+#         print(f"Arquivo salvo: {file.filename}, Citação: {citacao}")
+
+#         runApp(citacao)
+
+#     return {"message": "Todos os arquivos foram recebidos com sucesso!"}
 
 
 @app.route('/uploadlink', methods=['POST'])
--- settings_folders.py
+++ settings_folders.py
@@ -3,3 +3,4 @@ from pathlib import Path
 DOCS_DIR = Path("./docs")
 AUDIO_DIR = Path("./docs/audio")
 PDF_DIR = Path("./docs/pdf")
+IMG_DIR = Path("./docs/images")
